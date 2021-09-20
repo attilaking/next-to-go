b835c0d3f6f2a538744788fdda6009988996d7da
Max <hello@maxking.com.au>
2021-09-20 23:54:47 +1000

    Fix timer, add config

--- src/components/RacesList.js
+++ src/components/RacesList.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/components/racesList.scss';
 import Timer from '../components/Timer';
 
 const RacesList = (props) => {
     let listItems = "No data to display";
+    const [elapsedItems, setElapsedItems] = useState([]);
+
+    const onItemTimeElapsed = (id) => setElapsedItems(oldArray => [...oldArray, id]);
 
     if (props.ListData) {
-        listItems = props.ListData.map((item) =>
-            <tr key={item.race_id.toString()}>
-                <td>{item.meeting_name}</td>
-                <td>{item.race_number}</td>
-                <td><Timer StartTime={item.advertised_start.seconds}/></td>
-            </tr>
+
+        let sortedListData = props.ListData.sort((a, b) => parseInt(a.advertised_start.seconds) - parseInt(b.advertised_start.seconds));
+        let sortedFilteredListData = sortedListData.filter(item => !elapsedItems.includes(item.race_id) && item.categoryid == props.Config.selectedCategory)
+
+        listItems = sortedFilteredListData.map((item, index) => {
+            if(index < props.Config.maxNrOfItems){
+                return (
+                    <tr key={item.race_id.toString()}>
+                        <td>{item.meeting_name}</td>
+                        <td>{item.race_number}</td>
+                        <td><Timer OnTimeElapsed={(id) => onItemTimeElapsed(id)} StartTime={item.advertised_start.seconds} ItemId={item.race_id}/>
+                        </td>
+                    </tr>
+                    )
+            }
+            else {
+                return false;
+            }
+        }
         );
     }
     return (
